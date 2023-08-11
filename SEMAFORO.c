bd097033aa6a453d34afec68c63f2d241994f64a
Wesley00s <wesley300rodrigues@gmail.com>
2023-08-11 18:47:28 -0300

    ATT CORES

--- SEMAFORO.c
+++ SEMAFORO.c
@@ -4,10 +4,12 @@
 #include <unistd.h> // BIBLIOTECA PARA MANIPULAR O TEMPO
 
 // DEFININDO CORES
-#define VERMELHO "\x1B[31m"
-#define AMARELO "\x1B[33m"
-#define VERDE "\x1B[32m"
-#define PRETO "\x1B[30m"
+#define VERMELHO "\x1B[7;31m"
+#define VERMELHO_OFF "\x1B[31m"
+#define AMARELO "\x1B[7;33m"
+#define AMARELO_OFF "\x1B[33m"
+#define VERDE "\x1B[7;32m"
+#define VERDE_OFF "\x1B[32m"
 #define RESET "\x1B[0m"
 
 void fase1()
@@ -22,11 +24,11 @@ void fase1()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%s0%s\t=\t%sO%s", VERDE, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=", PRETO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO_OFF, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -50,11 +52,11 @@ void fase2()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=\t%s0%s", AMARELO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=", PRETO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO_OFF, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -78,11 +80,11 @@ void fase3()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -106,11 +108,11 @@ void fase3A()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -124,58 +126,58 @@ void fase3A()
 
 void fase3B1()
 {
-        printf("FASE 3B - (5s)\n\n"); // INICIO DA FASE 3B
-        printf("Rua Alfa\tRua Bravo\tPedestre\n");
-        printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, VERMELHO, RESET);
+    printf("FASE 3B - (5s)\n\n"); // INICIO DA FASE 3B
+    printf("Rua Alfa\tRua Bravo\tPedestre\n");
+    printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, VERMELHO, RESET);
 
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\tB\t\t=\t\t=");
-        printf("\n\t=================\t\t=================\n\n");
-        printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
-        printf("\n\t=================\t\t=================");
-        printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-        printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-        printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t^\t=");
-        printf("\n\t\t\t=\t|\t=");
-        printf("\n\t\t      A =\t|\t=");
-        printf("\n\n\n");
-        sleep(1);
-        system("clear");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\tB\t\t=\t\t=");
+    printf("\n\t=================\t\t=================\n\n");
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t=================\t\t=================");
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t^\t=");
+    printf("\n\t\t\t=\t|\t=");
+    printf("\n\t\t      A =\t|\t=");
+    printf("\n\n\n");
+    sleep(1);
+    system("clear");
 }
 
 void fase3B2()
 {
-        printf("FASE 3B - (5s)\n\n");
-        printf("Rua Alfa\tRua Bravo\tPedestre\n");
-        printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, PRETO, RESET);
+    printf("FASE 3B - (5s)\n\n");
+    printf("Rua Alfa\tRua Bravo\tPedestre\n");
+    printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, VERMELHO_OFF, RESET);
 
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\tB\t\t=\t\t=");
-        printf("\n\t=================\t\t=================\n\n");
-        printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
-        printf("\n\t=================\t\t=================");
-        printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-        printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-        printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t\t=");
-        printf("\n\t\t\t=\t^\t=");
-        printf("\n\t\t\t=\t|\t=");
-        printf("\n\t\t      A =\t|\t=");
-        printf("\n\n\n");
-        sleep(1);
-        system("clear");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\tB\t\t=\t\t=");
+    printf("\n\t=================\t\t=================\n\n");
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t=================\t\t=================");
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t\t=");
+    printf("\n\t\t\t=\t^\t=");
+    printf("\n\t\t\t=\t|\t=");
+    printf("\n\t\t      A =\t|\t=");
+    printf("\n\n\n");
+    sleep(1);
+    system("clear");
 }
 
 void fase4()
@@ -190,11 +192,11 @@ void fase4()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%sO%s  %sO%s  %s0%s\t\n\n", PRETO, RESET, PRETO, RESET, VERDE, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO_OFF, RESET, AMARELO_OFF, RESET, VERDE, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -218,11 +220,11 @@ void fase5()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%sO%s  %s0%s  %sO%s\t\n\n", PRETO, RESET, AMARELO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO_OFF, RESET, AMARELO, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -246,11 +248,11 @@ void fase6()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -274,11 +276,11 @@ void fase6A()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -302,11 +304,11 @@ void fase6B1()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERMELHO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -322,7 +324,7 @@ void fase6B2()
 {
     printf("FASE 6B - (5s)\n\n");
     printf("Rua Alfa\tRua Bravo\tPedestre\n");
-    printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, PRETO, RESET);
+    printf("%sVERMELHO%s\t%sVERMELHO%s\t%sVERMELHO%s\n\n\n", VERMELHO, RESET, VERMELHO, RESET, VERMELHO_OFF, RESET);
 
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -330,11 +332,11 @@ void fase6B2()
     printf("\n\t\t\t=\t\t=");
     printf("\n\tB\t\t=\t\t=");
     printf("\n\t=================\t\t=================\n\n");
-    printf("\n\t---->\t%s0%s  %sO%s  %sO%s\t\n\n", VERMELHO, RESET, PRETO, RESET, PRETO, RESET);
+    printf("\n\t----> \t%sO%s %sO%s %sO%s\t\n\n", VERMELHO, RESET, AMARELO_OFF, RESET, VERDE_OFF, RESET);
     printf("\n\t=================\t\t=================");
-    printf("\n\t\t\t=\t%sO%s\t=\t%s0%s", PRETO, RESET, VERDE, RESET);
-    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", PRETO, RESET, PRETO, RESET);
-    printf("\n\t\t\t=\t%s0%s\t=", VERMELHO, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", VERDE_OFF, RESET, VERDE_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=\t%sO%s", AMARELO_OFF, RESET, VERMELHO_OFF, RESET);
+    printf("\n\t\t\t=\t%sO%s\t=", VERMELHO, RESET);
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
     printf("\n\t\t\t=\t\t=");
@@ -361,6 +363,12 @@ int main()
             printf("O pedestre apertou o botão?\n1 - Não\n2 - Sim\n"); // DANDO A OPÇÃO DE APERTAR O BOTÃO PARA O PEDESTRE, PARA FASES (3A E 3B)
             scanf("%d", &SW1);
             system("clear"); // LIMPA O CONSOLE
+        }
+
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!\n\n" RESET);
+            break;
         }
 
         printf(VERDE "============================\n\n" RESET);
@@ -403,6 +411,12 @@ int main()
             system("clear");
         }
 
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!\n\n" RESET);
+            break;
+        }
+
         printf(VERMELHO "==\n\n" RESET);
         fase2();
         printf(VERMELHO "=\n\n" RESET);
@@ -415,6 +429,12 @@ int main()
             printf("O pedestre apertou o botão?\n1 - Não\n2 - Sim\n"); // VERIFICANDO SE O PEDESTRE APERTOU O BOTÃO, CASO NÃO, VERIFICA NOVAMENTE NA PRÓXIMA INTERAÇÃO
             scanf("%d", &SW1);
             system("clear");
+        }
+
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!\n\n" RESET);
+            break;
         }
 
         if (SW1 == 1)
@@ -442,7 +462,7 @@ int main()
             printf(VERMELHO "=\n\n" RESET);
             fase3A();
             system("clear");
-            
+
             printf(AMARELO "========\n\n" RESET);
             fase3B1();
             printf(VERMELHO "======\n\n" RESET);
@@ -462,6 +482,12 @@ int main()
             printf("O pedestre apertou o botão?\n1 - Não\n2 - Sim\n"); // DANDO A OPÇÃO DE APERTAR O BOTÃO PARA O PEDESTRE, PARA FASES (6A E 6B)
             scanf("%d", &SW1);
             system("clear");
+        }
+
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!" RESET);
+            break;
         }
 
         printf(VERDE "==================\n\n" RESET);
@@ -494,6 +520,12 @@ int main()
             system("clear");
         }
 
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!\n\n" RESET);
+            break;
+        }
+
         printf(VERMELHO "==\n\n" RESET);
         fase5();
         printf(VERMELHO "=\n\n" RESET);
@@ -506,6 +538,12 @@ int main()
             printf("O pedestre apertou o botão?\n1 - Não\n2 - Sim\n"); // DANDO A OPÇÃO DE APERTAR O BOTÃO PARA O PEDESTRE, PARA FASES (6A E 6B)
             scanf("%d", &SW1);
             system("clear");
+        }
+
+        if (SW1 != 1 && SW1 != 2)
+        {
+            printf(VERMELHO "[ERROR]Opção inválida!" RESET);
+            break;
         }
 
         if (SW1 == 1)
